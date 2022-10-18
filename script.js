371ee928f43d1e0d41b7f12ea3c9ee84ac88ad8e
emines0 <emin.sefa1@gmail.com>
2022-10-18 19:23:51 +0200

    test3

--- script.js
+++ script.js
@@ -2,20 +2,26 @@ const videoElement = document.getElementById('video');
 const button = document.getElementById('button');
 
 // Prompt to select media stream, pass to video element, then play
-
 async function selectMediaStream() {
-    try {
-        const mediaStream = await navigator.mediaDevices.getDisplayMedia(displayMediaOptions);
-        videoElement.srcObject = mediaStream;
-        videoElement.onloadedmetadata = () => {
-            videoElement.play();
-        }
-        
-    } catch (error) {
-        console.error('Error: ', error);
-    }
+  try {
+    const mediaStream = await navigator.mediaDevices.getDisplayMedia();
+    videoElement.srcObject = mediaStream;
+    videoElement.onloadedmetadata = () => {
+      videoElement.play();
+    };
+  } catch (error) {
+    // Catch Error Here
+  }
 }
 
-//On load
+button.addEventListener('click', async () => {
+  // Disable Button
+  button.disabled = true;
+  // Start Picture in Picture
+  await videoElement.requestPictureInPicture();
+  // Reset Button
+  button.disabled = false;
+});
 
+// On Load
 selectMediaStream();
